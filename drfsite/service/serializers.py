3621dae8cd71bfcaf7d840922c37101c4f1d5b1a
Aleksandr <medovnikaleksander@yandex.ua>
2022-10-27 16:54:34 +0300

    added encode & decode

--- drfsite/service/serializers.py
+++ drfsite/service/serializers.py
@@ -1,11 +1,50 @@
+import io
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from .models import Post
 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('title', 'content', 'cat_id', 'time_create', 'time_update', 'is_published')
+class PostSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# class PostModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+# class PostSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=150)
+#     content = serializers.CharField()
+
+
+# def encode():
+#     model = PostModel('Title 1', 'Post 1')
+#     model_sr = PostSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+
+
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Title 1","content":"Post 1"}')
+#     data = JSONParser().parse(stream)
+#     serializer = PostSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
+
+
+# class PostSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Post
+#         fields = ('title', 'content', 'cat_id', 'time_create', 'time_update', 'is_published')
 
 # class PostSerializer(serializers.Serializer):
 #     title = serializers.CharField(max_length=255)
--- drfsite/service/views.py
+++ drfsite/service/views.py
@@ -8,63 +8,15 @@ from django.forms import model_to_dict
 
 class PostAPIView(APIView):
     def get(self, request):
-        lst = Post.objects.all().values()
-        return Response({'posts': list(lst)})
+        p = Post.objects.all()
+        return Response({'post': PostSerializer(p, many=True).data})
 
     def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         post_new = Post.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
-        return Response({'post': model_to_dict(post_new)})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method PUT not allowed'})
-        try:
-            instance = Post.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object does not exists'})
-        instance.title = request.data['title']
-        instance.content = request.data['content']
-        instance.cat_id = request.data['cat_id']
-        instance.save()
-        return Response({'post': request.data})
-
-
-#
-#
-# class PostDelete(APIView):
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get('pk', None)
-#         if not pk:
-#             return Response({'error': 'Method DELETE not allowed'})
-#         Post.objects.get(pk=pk).delete()
-#         return Response({'post': f'Delete post {str(pk)}'})
-
-
-'''class PostAPIView(APIView):
-    def get(self, request):
-        l = Post.objects.all()
-        return Response({'post': PostSerializer(l, many=True).data})
-
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post': serializer.data})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method put not allowed'})
-        try:
-            instance = Post.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object not...'})
-        serializer = PostSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post': serializer.data})'''
+        return Response({'post': PostSerializer(post_new).data})
