b713b74fc7272c5f5e2e2ba10196b9d9a86b7ced
Aleksandr <medovnikaleksander@yandex.ua>
2022-11-01 16:36:24 +0300

    added post & put

--- drfsite/service/serializers.py
+++ drfsite/service/serializers.py
@@ -1,7 +1,4 @@
-import io
 from rest_framework import serializers
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from .models import Post
 
 
@@ -13,32 +10,19 @@ class PostSerializer(serializers.Serializer):
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
 
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
 
-# class PostModel:
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
-
-
-# class PostSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=150)
-#     content = serializers.CharField()
-
-
-# def encode():
-#     model = PostModel('Title 1', 'Post 1')
-#     model_sr = PostSerializer(model)
-#     print(model_sr.data, type(model_sr.data), sep='\n')
-#     json = JSONRenderer().render(model_sr.data)
-#     print(json)
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.time_update = validated_data.get('time_update', instance.time_update)
+        instance.is_published = validated_data.get('is_published', instance.is_published)
+        instance.cat_id = validated_data.get('cat_id', instance.cat_id)
+        instance.save()
+        return instance
 
 
-# def decode():
-#     stream = io.BytesIO(b'{"title":"Title 1","content":"Post 1"}')
-#     data = JSONParser().parse(stream)
-#     serializer = PostSerializer(data=data)
-#     serializer.is_valid()
-#     print(serializer.validated_data)
 
 
 # class PostSerializer(serializers.ModelSerializer):
@@ -46,22 +30,4 @@ class PostSerializer(serializers.Serializer):
 #         model = Post
 #         fields = ('title', 'content', 'cat_id', 'time_create', 'time_update', 'is_published')
 
-# class PostSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=255)
-#     content = serializers.CharField()
-#     time_create = serializers.DateTimeField(read_only=True)
-#     time_update = serializers.DateTimeField(read_only=True)
-#     is_published = serializers.BooleanField(default=True)
-#     cat_id = serializers.IntegerField()
-#
-#     def create(self, validated_data):
-#         return Post.objects.create(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.content = validated_data.get('content', instance.content)
-#         instance.time_update = validated_data.get('time_update', instance.time_update)
-#         instance.is_published = validated_data.get('is_published', instance.is_published)
-#         instance.cat_id = validated_data.get('cat_id', instance.cat_id)
-#         instance.save()
-#         return instance
+
--- drfsite/service/views.py
+++ drfsite/service/views.py
@@ -12,27 +12,18 @@ class PostAPIView(APIView):
     def post(self, request):
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        post_new = Post.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': PostSerializer(post_new).data})
+        serializer.save()
+        return Response({'post': serializer.data})
 
-
-    # ДЗ
-    # def put(self, request, *args, **kwargs):
-    #     pk = kwargs.get('pk', None)
-    #     if not pk:
-    #         return Response({'error': 'Method PUT not allowed'})
-    #     try:
-    #         instance = Post.objects.get(pk=pk)
-    #     except:
-    #         return Response({'error': 'Object does not exists'})
-    #     serializer = PostSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     instance.title = request.data['title']
-    #     instance.content = request.data['content']
-    #     instance.cat_id = request.data['cat_id']
-    #     instance.save()
-    #     return Response({'post': PostSerializer(instance).data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+        try:
+            instance = Post.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Object does not exists'})
+        serializer = PostSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
