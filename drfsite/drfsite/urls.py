2765d8ecd0d8cef714ec8b889e4fb5eb81802195
Aleksandr <medovnikaleksander@yandex.ua>
2022-11-03 15:47:43 +0300

    added methods & homework

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from service.views import PostAPIView
-
+from service.views import PostAPIList, PostAPIUpdate, PostAPIDestroy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/postlist/', PostAPIView.as_view()),
-    path('api/v1/postlist/<int:pk>/', PostAPIView.as_view()),
+    path('api/v1/postlist/', PostAPIList.as_view()),
+    path('api/v1/postlist/<int:pk>/', PostAPIUpdate.as_view()),
+    path('api/v1/postdestroy/<int:pk>/', PostAPIDestroy.as_view()),
 ]
--- drfsite/service/views.py
+++ drfsite/service/views.py
@@ -1,36 +1,18 @@
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 
-class PostAPIView(APIView):
-    def get(self, request):
-        p = Post.objects.all()
-        return Response({'posts': PostSerializer(p, many=True).data})
+class PostAPIList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post': serializer.data})
 
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method PUT not allowed'})
-        try:
-            instance = Post.objects.get(pk=pk)
-        except:
-            return Response({'error': 'Object does not exists'})
-        serializer = PostSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post': serializer.data})
+class PostAPIUpdate(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def delete(self, request, *argsl, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'error': 'Method DELETE not allowed'})
-        Post.objects.get(pk=pk).delete()
-        return Response({'post': f'Delete post {str(pk)}'})
+
+class PostAPIDestroy(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
